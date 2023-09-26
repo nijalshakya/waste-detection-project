53a088466424122ad6188716ccf6f01d235752f0
nijal shakya <nijalshakya1111ns@gmail.com>
2023-09-26 09:24:10 +0545

    templates and requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'nijal shakya',
+    author_email= 'nijalshakya1111ns@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
