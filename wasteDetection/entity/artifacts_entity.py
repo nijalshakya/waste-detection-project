14ff8a7f3a56ab05120c46c41ca4e10028f3c347
nijal shakya <nijalshakya1111ns@gmail.com>
2023-10-05 11:18:50 +0545

    model trainer added

--- wasteDetection/entity/artifacts_entity.py
+++ wasteDetection/entity/artifacts_entity.py
@@ -9,3 +9,10 @@ class DataIngestionArtifact:
 @dataclass
 class DataValidationArtifact:
     validation_status: bool
+
+
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
